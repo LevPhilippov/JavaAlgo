ed1523304c6ac771b6fdb517a487fc90c6cadabd
LevPhilippov <filippov.l.a.89@gmail.com>
2019-02-27 00:06:39 +0500

    Переделываю класс дэк и очередь. Остановился на том, что при создании экземпляра очереди конструктор с параметром size создает массив data размером в 2 раза больше!

--- JavaAlgo_Lesson3_Stack_Queue_Deque/src/lev/filippov/Queue.java
+++ JavaAlgo_Lesson3_Stack_Queue_Deque/src/lev/filippov/Queue.java
@@ -1,34 +1,39 @@
 package lev.filippov;
 
+import java.util.Arrays;
+
 public class Queue <E> implements QueueInt <E> {
 
-    public static final int FRONT = -1;
-    public static final int END = 0;
-    public static final int SIZE = -1;
-    private int end;
-    private int front;
-    private static final int maxSize = 16;
-    private int size;
-    private E[] data;
+    static final int FRONT = 0;
+    static final int END = -1;
+    static final int SIZE = 0;
+    int end;
+    int front;
+    static final int MAX_SIZE = 16;
+    int size;
+    E[] data;
 
     public Queue (int size) {
         end = END;
         front = FRONT;
-        size = SIZE;
+        this.size = size;
         data = (E[])new Object[size];
     }
 
     public Queue () {
-      this(maxSize);
+      this(MAX_SIZE);
     }
 
     @Override
     public void push(E value) {
         if (isFull())
             throw new RuntimeException();
-        if (end == data.length-1)
-            end = END; //0
-        data[end++] = value;
+
+        if (end == data.length) {
+            end = END; //-1
+        }
+
+        data[++end] = value;
         size++;
     }
 
@@ -38,15 +43,15 @@ public class Queue <E> implements QueueInt <E> {
             throw new RuntimeException();
         size--;
         if (front == data.length)
-            front = FRONT; //-1
-        return data[++front] ;
+            front = FRONT; //0
+        return data[front++] ;
     }
 
     @Override
     public E peek() {
         if (isEmpty())
             throw new RuntimeException();
-        return data[end];
+        return data[front];
     }
 
     @Override
@@ -62,5 +67,9 @@ public class Queue <E> implements QueueInt <E> {
     @Override
     public int getSize() {
         return size;
+    }
+
+    public String display(){
+        return Arrays.toString(data);
     }
 }
